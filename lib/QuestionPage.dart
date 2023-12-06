f102a4ed375a931fc936301eeafb49eeae2da8c3
runam00 <runam00@users.noreply.github.com>
2023-12-06 11:20:30 +0900

    ランダムな画像アイテムを返す関数を追加

--- lib/QuestionPage.dart
+++ lib/QuestionPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'ResultPage.dart';
+import 'dart:math';
 
 class QuestionPage extends StatelessWidget{
   @override
@@ -9,24 +10,50 @@ class QuestionPage extends StatelessWidget{
         title : Text("質問")
       ),
       body : Center(
-        child: ElevatedButton(
-          child: Text("結果を表示"),
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue,
-            onPrimary: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10)
+        child: Column(mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+
+            Container(
             ),
-          ),
-          onPressed: (){
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ResultPage(),
-              ));
-          },
+
+            // 結果を表示するボタン
+            Container(
+              child: ElevatedButton(
+                child: Text("結果を表示"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ResultPage(generateRandomImageItem()),
+                    )
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       )
     );
+  }
+
+  // 画像のパスをランダムに返す
+  Map<String, String> generateRandomImageItem(){
+    List<Map<String, String>> image_paths = [
+      {'ポテトチップス': 'images/potatochips.png'},
+      {'キャンディ': 'images/sweets_candy.png'},
+      {'クラッカー': 'images/sweets_cracker.png'},
+      {'グミ': 'images/sweets_gumi.png'},
+      {'カキピー': 'images/sweets_kakipi.png'},
+      {'チョコレート': 'images/valentine_chocolates.png'},
+    ];
+    var random = new Random();
+    return image_paths[random.nextInt(image_paths.length)];
   }
 }
